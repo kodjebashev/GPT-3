1313bd8ee4a7a6655d0ac765474b1a45917d9914
Georgi Kodjebashev <gk25371271@edu.mon.bg>
2023-06-04 09:02:02 +0300

    First commit

---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./whargpt3.css";
+
+const WharGPT3 = () =>{
+    return(
+        <div>
+            WharGPT3
+        </div>
+    )
+}
+
+export default WharGPT3
